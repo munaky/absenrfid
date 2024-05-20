73e68b7eee54988da1bf49732e34dc301faac53a
munaky <121778708+munaky@users.noreply.github.com>
2024-05-20 21:38:52 +0700

    Update

--- absenrfid_esp8266/absenrfid_esp8266.ino
+++ absenrfid_esp8266/absenrfid_esp8266.ino
@@ -20,7 +20,7 @@ LiquidCrystal_I2C_PLUS lcd(0x27,16,2);
 DynamicJsonDocument doc(1024);
 
 //Host
-const char host[] = "192.168.25.201";
+const char host[] = "example.com";
 const int port = 8000;
 const String tokenV = "/api/validToken";
 const String mainUrl = "/api/rfid";
